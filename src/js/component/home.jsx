058642f414bc26cd524bad66b340aa3b1ef5fc65
krocksa <rogeryonatan@gmail.com>
2022-09-08 20:16:16 +0000

    Actualization 3

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 		<>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">Start Bootstrap</a>
+    <div class="navbar-brand" href="#">Start Bootstrap</div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -32,31 +32,83 @@ const Home = () => {
   </div>
 </nav>
 <div class="container head">
-	<h1>
+	<h2>
 		A Warm Welcome!
-	</h1>
-	<br />
-	<h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolor iusto dicta voluptate reprehenderit, dignissimos architecto ipsa explicabo consequatur nesciunt doloremque impedit inventore placeat cum reiciendis ea, veritatis tempora. Error.</h5><br />
+	</h2>
+	<h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolor iusto dicta voluptate reprehenderit, dignissimos architecto ipsa explicabo consequatur nesciunt doloremque impedit inventore placeat cum reiciendis ea, veritatis tempora. Error.</h6>
 	<button type="button" class="btn btn-primary btn-lg">Call to action!</button>
 </div>
 <div class=" container body">
     <div class="row row-max">
       <div class="col col-max">
         <div class="row row-int">
-          <div class="col col-int">
-
+          <div class="col-sm-3 col-int">
+              <div class="row row-3">
+                <div class="col-sm-4 body-head">
+                  500 X 325
+                </div>
+                <div class="col-sm-6 body-center">
+                  <br />
+                  <h4>Card Title</h4>
+                  <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, sunt voluptatem excepturi eaque aliquid.</h6>
+                </div>
+                <div class="col-sm-2 body-footer">
+                    <button type="button" class="btn btn-primary boot">Find Out More!</button>
+                </div>
+              </div>
           </div>
-      <div class="col col-int">
+      <div class="col-sm-3 col-int">
+      <div class="row row-3">
+                <div class="col-sm-4 body-head">
+                  500 X 325
+                </div>
+                <div class="col-sm-6 body-center">
+                  <br />
+                  <h4>Card Title</h4>
+                  <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, sunt voluptatem excepturi eaque aliquid.</h6>
+                </div>
+                <div class="col-sm-2 body-footer">
+                    <button type="button" class="btn btn-primary boot">Find Out More!</button>
+                </div>
+              </div>
       </div>
-      <div class="col col-int">
+      <div class="col-sm-3 col-int">
+      <div class="row row-3">
+                <div class="col-sm-4 body-head">
+                  500 X 325
+                </div>
+                <div class="col-sm-6 body-center">
+                  <br />
+                  <h4>Card Title</h4>
+                  <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, sunt voluptatem excepturi eaque aliquid.</h6>
+                </div>
+                <div class="col-sm-2 body-footer">
+                    <button type="button" class="btn btn-primary boot">Find Out More!</button>
+                </div>
+              </div>
       </div>
-      <div class="col col-int">
+      <div class="col-sm-3 col-int">
+      <div class="row row-3">
+                <div class="col-sm-4 body-head">
+                  500 X 325
+                </div>
+                <div class="col-sm-6 body-center">
+                  <br />
+                  <h4>Card Title</h4>
+                  <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, sunt voluptatem excepturi eaque aliquid.</h6>
+                </div>
+                <div class="col-sm-2 body-footer">
+                    <button type="button" class="btn btn-primary boot">Find Out More!</button>
+                </div>
+              </div>
       </div>
         </div>
     </div>
       </div>
 </div>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand nav-footer" href="#">Copyriht &copy; Your Website 2022</a>
+</nav>
 		</>
 	);
 };
